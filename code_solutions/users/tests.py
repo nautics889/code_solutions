644457d339e4a24417ea5d1a80b9b01204e99d73
quantum <cyberukr@gmail.com>
2019-05-06 13:58:02 +0300

    implemented: test-coverage of existing endpoints

--- code_solutions/users/tests.py
+++ code_solutions/users/tests.py
@@ -1,3 +1,42 @@
+import json
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APIRequestFactory, APIClient
 
-# Create your tests here.
+from users.models import User
+from rest_framework_simplejwt import views as jwt_views
+
+
+#fixtures must be moved outside of this file further
+USER = {
+	"username": "foobar",
+	"email": "foobar@example.com",
+	"password": "newpassword",
+	"first_name": "fn",
+	"last_name": "ln"
+}
+
+
+class BasicTestCase(TestCase):
+    factory = APIRequestFactory()
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.user = User.objects.create_user(**USER)
+
+
+class CurrentUserRetrieveUpdateTestCase(BasicTestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+
+    def test_retrieve_info_about_logged_user(self):
+        response = self.client.get('/users/details/', format='json')
+        self.assertEqual(response.data.get('username'), 'foobar')
+        self.client.logout()
+
+    def test_update_field_of_logged_user(self):
+        self.client.patch('/users/details/', {'username': 'FOOBAR'}, format='json')
+        self.assertTrue(User.objects.get(id=self.user.id).username.isupper())
+        self.client.logout()
